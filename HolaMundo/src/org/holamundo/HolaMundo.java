818fd59a037337eb6fea6db9a3e5ffe2545c8a11
francarceles <61408458+francarceles@users.noreply.github.com>
2020-04-21 01:21:33 +0200

    Añadido comentario

--- HolaMundo/src/org/holamundo/HolaMundo.java
+++ HolaMundo/src/org/holamundo/HolaMundo.java
@@ -3,8 +3,8 @@ package org.holamundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		//Este comentario ha sido añadido por FranCarceles.
 		System.out.println("Hola Mundo");
-
 	}
 
 }
